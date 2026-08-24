cd76cd97a424bf963d892c7c19e50bd203edd089
lining-lo <2092576148@qq.com>
2026-08-24 20:20:57 +0800

    feat: 搭建DialogueEngine流程框架

--- customer-service-backend/app/engine/dialogue_engine.py
+++ customer-service-backend/app/engine/dialogue_engine.py
@@ -3,7 +3,74 @@
   @Time:2026/8/17
   @Desc: 
 """
+import uuid
+from app.domain.message import UserMessage, ProcessResult, MessageType, BotMessage
+from app.domain.state import DialogueState, Turn
+from app.plan.turn_plan import TurnPlan
+from app.plan.turn_plan_validation import TurnPlanValidation
+from app.task.handler import TaskHandler
+
+
 # 处理消息
 class DialogueEngine:
-    async def process_mesasge(self, state, user_message):
+    def __init__(self,
+                 turn_plan: TurnPlan,
+                 turn_plan_validation: TurnPlanValidation,
+                 task_handler: TaskHandler):
+        self._turn_plan = turn_plan
+        self._turn_plan_validation = turn_plan_validation
+        self._task_handler = task_handler
+
+    async def process_mesasge(self, state: DialogueState,
+                              user_message: UserMessage) -> ProcessResult:
+        # 1 准备当前会话
+        self._prepare_session(state)
+
+        # 2 准备本轮Turn
+        turn = Turn(turn_id=str(uuid.uuid4()), user_message=user_message)
+
+        # 3 判断消息类型
+        # 文本类型消息
+        if user_message.type == MessageType.TEXT:
+            messages: list[BotMessage] = self._execute_text_message(user_message, state)
+        else:  # 对象类型消息
+            messages: list[BotMessage] = self._execute_object_message(user_message, state)
+
+        # 4 提交本轮对话记录
+        ## 封装list[BotMessage]到turn对象
+        turn.bot_message.extend(messages)
+        # 放到当前session里面
+        state.shared.sessions[-1].turns.append(turn)
+
+        # 5 返回本轮回复
+        return ProcessResult(
+            sender_id=user_message.sender_id,
+            message_id=user_message.message_id,
+            messages=messages
+        )
+
+    # 1 准备当前session会话
+    def _prepare_session(self, state: DialogueState):
+        pass
+
+    # 2 处理文本类型消息
+    def _execute_text_message(self,
+                              user_message: UserMessage,
+                              state: DialogueState) -> list[BotMessage]:
+        # 1 根据user_message文本提问信息，调用llm，进行意图识别
+        # 识别执行哪个轨道：任务流程、知识检索、闲聊；
+        # 如果任务流程，识别流程id
+
+        # 2 对llm意图识别结果校验
+        ## 比如识别有两个轨道，任务流程识别流程id不存在......
+
+        # 3 校验失败，调用反问澄清组件
+
+        # 4 校验成功，根据识别不同轨道，调用不同handler处理，
+        # 比如识别任务流程，调用TaskHandler方法执行
+
+        pass
+
+    # 处理对象类型消息
+    def _execute_object_message(self, user_message, state) -> list[BotMessage]:
         pass
---
+++ customer-service-backend/app/plan/turn_plan.py
@@ -0,0 +1,10 @@
+"""
+  @Author:lining-lo
+  @Time:2026/8/24
+  @Desc:
+"""
+
+
+# 意图识别组件
+class TurnPlan:
+    pass
